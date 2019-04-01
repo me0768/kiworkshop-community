e1ff65af6282577dc40d793c2ecb2e16f4a1b249
myang <kmh4503@gmail.com>
2019-04-02 00:14:25 +0900

    [INITIAL] FE vue cli & vuetify install

---
+++ frontend/community/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>광일공방</span>
+      </v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+    data() {
+      return {
+        //
+      }
+    }
+  }
+</script>
